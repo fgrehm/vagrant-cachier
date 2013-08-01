8203161d6d4b1b2d4bc3fd96300cc0992882884e
Teemu Matilainen <teemu.matilainen@iki.fi>
2013-08-01 22:35:12 +0300

    Don't error out if a bucket is configured for a non-capable guest
    If a bucket is configured (globally), just print an info level message
    when configuring a non-capable guest machine.

--- lib/vagrant-cachier/bucket/yum.rb
+++ lib/vagrant-cachier/bucket/yum.rb
@@ -27,8 +27,7 @@ module VagrantPlugins
               end
             end
           else
-            # TODO: Raise a better error
-            raise "You've configured a Yum cache for a guest machine that does not support it!"
+            @env[:ui].info "Skipping Yum cache bucket as the guest machine does not support it"
           end
         end
       end
