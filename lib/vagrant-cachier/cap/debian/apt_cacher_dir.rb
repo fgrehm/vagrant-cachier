7bc70b7d6eaebd983eafc79cbefd1533874e12c8
Fabio Rehm <fgrehm@gmail.com>
2013-10-26 15:47:31 -0200

    Do not attempt to configure apt-cacher-ng bucket if it is not installed on the guest machine

--- lib/vagrant-cachier/cap/debian/apt_cacher_dir.rb
+++ lib/vagrant-cachier/cap/debian/apt_cacher_dir.rb
@@ -3,9 +3,18 @@ module VagrantPlugins
     module Cap
       module Debian
         module AptCacherDir
+          CACHER_CONF      = '/etc/apt-cacher-ng/acng.conf'
+          CACHER_CACHE_DIR = "$(cat #{CACHER_CONF} | grep CacheDir | cut -d' ' -f 2)"
+
           def self.apt_cacher_dir(machine)
-            # cat /etc/apt-cacher-ng/acng.conf |grep CacheDir
-            '/var/cache/apt-cacher-ng'
+            cache_dir = nil
+            machine.communicate.tap do |comm|
+              return unless comm.test("test -f #{CACHER_CONF}")
+              comm.execute "echo #{CACHER_CACHE_DIR}" do |buffer, output|
+                cache_dir = output.chomp if buffer == :stdout
+              end
+            end
+            return cache_dir
           end
         end
       end
