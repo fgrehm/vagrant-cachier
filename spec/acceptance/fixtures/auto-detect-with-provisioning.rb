5e5070dc14be5de37f8fcacac40bcfb6b0df87c9
Ealden Esto E. Escañan <ealden@escanan.com>
2013-12-05 22:50:17 +0800

    Vagrant should be able to download precise64 if we don't have it

--- spec/acceptance/fixtures/auto-detect-with-provisioning.rb
+++ spec/acceptance/fixtures/auto-detect-with-provisioning.rb
@@ -5,5 +5,6 @@ Vagrant.configure("2") do |config|
   config.cache.scope = :machine
 
   config.vm.box = 'precise64'
+  config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
   config.vm.provision :shell, inline: 'apt-get update && apt-get install -y git'
 end
--- spec/acceptance/fixtures/auto-detect.rb
+++ spec/acceptance/fixtures/auto-detect.rb
@@ -3,5 +3,6 @@ Vagrant.require_plugin 'vagrant-lxc'
 Vagrant.configure("2") do |config|
   config.cache.auto_detect = true
   config.vm.box = 'precise64'
+  config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
   config.vm.provision :shell, inline: 'echo Hello!'
 end
