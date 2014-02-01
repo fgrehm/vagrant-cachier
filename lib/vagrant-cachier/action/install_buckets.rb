dc2cb5eb79f485e55706484a041c422cf7b6d566
Fabio Rehm <fgrehm@gmail.com>
2014-02-01 18:12:38 -0200

    Clean up InstallBuckets action a bit

--- lib/vagrant-cachier/action/install_buckets.rb
+++ lib/vagrant-cachier/action/install_buckets.rb
@@ -11,28 +11,27 @@ module VagrantPlugins
         def call(env)
           @app.call(env)
 
-          @env = env
-          configure_cache_buckets
+          configure_cache_buckets(env)
         end
 
         def configure_cache_buckets
-          return unless @env[:machine].config.cache.enabled?
+          return unless env[:machine].config.cache.enabled?
 
-          if @env[:machine].config.cache.auto_detect
-            Bucket.auto_detect(@env)
+          if env[:machine].config.cache.auto_detect
+            Bucket.auto_detect(env)
           end
 
-          return unless @env[:machine].config.cache.buckets.any?
+          return unless env[:machine].config.cache.buckets.any?
 
-          @env[:ui].info 'Configuring cache buckets...'
-          cache_config = @env[:machine].config.cache
+          env[:ui].info 'Configuring cache buckets...'
+          cache_config = env[:machine].config.cache
           cache_config.buckets.each do |bucket_name, configs|
             # cachier_debug "Installing #{bucket_name} with configs #{configs.inspect}"
-            Bucket.install(bucket_name, @env, configs)
+            Bucket.install(bucket_name, env, configs)
           end
 
-          data_file = @env[:machine].data_dir.join('cache_dirs')
-          data_file.open('w') { |f| f.print @env[:cache_dirs].uniq.join("\n") }
+          data_file = env[:machine].data_dir.join('cache_dirs')
+          data_file.open('w') { |f| f.print env[:cache_dirs].uniq.join("\n") }
         end
       end
     end
