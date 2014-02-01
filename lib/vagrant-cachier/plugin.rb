e01a78138aaa946b2a85780d3ce8665b6f6b04c3
Fabio Rehm <fgrehm@gmail.com>
2014-02-01 16:18:58 -0200

    Blow up if using vagrant < 1.4.0

--- lib/vagrant-cachier/plugin.rb
+++ lib/vagrant-cachier/plugin.rb
@@ -1,3 +1,9 @@
+# TODO: Switch to Vagrant.require_version before 1.0.0
+#       see: https://github.com/mitchellh/vagrant/blob/bc55081e9ffaa6820113e449a9f76b293a29b27d/lib/vagrant.rb#L202-L228
+unless Gem::Requirement.new('>= 1.4.0').satisfied_by?(Gem::Version.new(Vagrant::VERSION))
+  raise 'vagrant-cachier requires Vagrant >= 1.4.0 in order to work!'
+end
+
 require_relative 'provision_ext'
 Vagrant::Action::Builtin::Provision.class_eval do
   include VagrantPlugins::Cachier::ProvisionExt
