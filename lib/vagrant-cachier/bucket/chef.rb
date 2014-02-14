437ba6a4cf162c65e5c563a173ff8b64baf1c0f9
Fabio Rehm <fgrehm@gmail.com>
2014-02-14 01:15:43 -0200

    :warning: Massive refactoring of buckets code :warning:
    Doing this is kinda irresponsible because we don't have any unit testing
    in place but I've had enough of copy & pasting things around. Although it
    doesn't make the codebase GREAT, I believe it'll reach a _nice_ status :)
    
    Thanks to those changes I realized that skipping configuration of
    buckets that have already been configured was easier than I thought and
    should be enough to close GH-85 \o/

--- lib/vagrant-cachier/bucket/chef.rb
+++ lib/vagrant-cachier/bucket/chef.rb
@@ -7,22 +7,9 @@ module VagrantPlugins
         end
 
         def install
-          machine = @env[:machine]
-          guest   = machine.guest
-
           if guest.capability?(:chef_file_cache_path)
             guest_path = guest.capability(:chef_file_cache_path)
-
-            @env[:cache_dirs] << guest_path
-
-            machine.communicate.tap do |comm|
-              comm.execute("mkdir -p /tmp/vagrant-cache/#{@name}")
-              unless comm.test("test -L #{guest_path}")
-                comm.sudo("rm -rf #{guest_path}")
-                comm.sudo("mkdir -p `dirname #{guest_path}`")
-                comm.sudo("ln -s /tmp/vagrant-cache/#{@name} #{guest_path}")
-              end
-            end
+            symlink(guest_path) if guest_path
           else
             @env[:ui].info I18n.t('vagrant_cachier.skipping_bucket', bucket: 'Chef')
           end
