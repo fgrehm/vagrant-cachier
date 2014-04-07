a4c5f0526388feecd0cb77a6ba5b28a5843d81be
Fabio Rehm <fgrehm@gmail.com>
2014-04-06 21:26:26 -0300

    Add missing `end` to generic bucket and remove `capability` method [GH-94]

--- lib/vagrant-cachier/bucket/generic.rb
+++ lib/vagrant-cachier/bucket/generic.rb
@@ -2,16 +2,13 @@ module VagrantPlugins
   module Cachier
     class Bucket
       class Generic < Bucket
-        def self.capability
-          :generic_cache_dir
-        end
-
         def install
           if @configs.has_key?(:cache_dir)
             @name = @configs.has_key?(:name) ? "generic-#{@configs[:name]}" : "generic"
             symlink(@configs[:cache_dir])
           else
             @env[:ui].info I18n.t('vagrant_cachier.skipping_bucket', bucket: 'Generic')
+          end
         end
       end
     end
