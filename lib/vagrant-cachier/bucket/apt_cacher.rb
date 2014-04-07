12f121e6e452f36cc276b51cb8de66ae1103bf25
Fabio Rehm <fgrehm@gmail.com>
2014-04-06 21:47:24 -0300

    Properly fix GH-96

--- lib/vagrant-cachier/bucket/apt_cacher.rb
+++ lib/vagrant-cachier/bucket/apt_cacher.rb
@@ -11,11 +11,11 @@ module VagrantPlugins
         def install
           if guest.capability?(:apt_cacher_dir)
             if guest_path = guest.capability(:apt_cacher_dir)
-              synced_folder_type = machine.config.cache.synced_folder_opts[:type]
+              synced_folder_type = (machine.config.cache.synced_folder_opts || {})[:type]
               if synced_folder_type && synced_folder_type.to_sym == :nfs
                 symlink(guest_path)
               else
-                @env[:ui].info I18n.t('vagrant_cachier.nfs_required', bucket: 'apt-cacher')
+                @env[:ui].warn I18n.t('vagrant_cachier.nfs_required', bucket: 'apt-cacher')
               end
             end
           else
