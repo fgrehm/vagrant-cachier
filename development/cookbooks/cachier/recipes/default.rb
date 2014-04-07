ff5dd72c736a314c64c3a5cac6819452859dc1fc
Fabio Rehm <fgrehm@gmail.com>
2014-04-06 22:28:15 -0300

    Download a specific version of vagrant-cachier tarball instead of master so that it can be cached properly

--- development/cookbooks/cachier/recipes/default.rb
+++ development/cookbooks/cachier/recipes/default.rb
@@ -1,4 +1,4 @@
-zip_filepath = "#{Chef::Config['file_cache_path']}/vagrant-cachier.zip"
-zip_url      = "https://github.com/fgrehm/vagrant-cachier/archive/master.zip"
+zip_filepath = "#{Chef::Config['file_cache_path']}/vagrant-cachier.tar.gz"
+zip_url      = "https://github.com/fgrehm/vagrant-cachier/archive/v0.6.0.tar.gz"
 
 remote_file(zip_filepath) { source zip_url }
